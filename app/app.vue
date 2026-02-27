3e8baf26a9674faec52a20bce8a3c90a466cc268
Arnaud Gaydamour <arnaud.gaydamour@protonmail.com>
2026-02-27 22:56:49 +0100

    feat: add a toaster notification on mass delete (#58)

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,11 @@
+<script setup lang="ts">
+import { Toaster } from '@/components/ui/sonner'
+</script>
+
 <template>
   <NuxtRouteAnnouncer />
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
+  <Toaster />
 </template>
